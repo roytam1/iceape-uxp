12f5dcbb5f6ed6892a93a2f4ad09ab2b3d208f4d
Gaming4JC <g4jc@hyperbola.info>
2019-08-30 09:32:21 -0400

    backport c-c 1569830 - Port Bug 1120967 "Broken middle/right click on links via about:preferences pages" to Iceape-UXP.

--- common/nsContextMenu.js
+++ common/nsContextMenu.js
@@ -518,7 +518,9 @@ nsContextMenu.prototype = {
 
     const xulNS = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
 
-    if (aNode.namespaceURI == xulNS) {
+    if (aNode.nodeType == Node.DOCUMENT_NODE ||
+        // Not display on XUL element but relax for <label class="text-link">
+        (aNode.namespaceURI == xulNS && !isXULTextLinkLabel(aNode))) {
       this.shouldDisplay = false;
       return;
     }
@@ -683,7 +685,8 @@ nsContextMenu.prototype = {
       if (elem.nodeType == Node.ELEMENT_NODE) {
         // Link?
         if (!this.onLink &&
-            ((elem instanceof HTMLAnchorElement && elem.href) ||
+            (isXULTextLinkLabel(elem) ||
+             (elem instanceof HTMLAnchorElement && elem.href) ||
              (elem instanceof HTMLAreaElement && elem.href) ||
              elem instanceof HTMLLinkElement ||
              (elem.namespaceURI == NS_MathML && elem.hasAttribute("href")) ||
@@ -771,6 +774,13 @@ nsContextMenu.prototype = {
           this.showItem("spell-separator", canSpell);
         }
       }
+    }
+
+    function isXULTextLinkLabel(node) {
+      return node.namespaceURI == xulNS &&
+             node.tagName == "label" &&
+             node.classList.contains('text-link') &&
+             node.href;
     }
   },
 
